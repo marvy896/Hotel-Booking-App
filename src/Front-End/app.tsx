3b1940ed0d077184e161eb8f5add275f88587add
marvy896 <onyex896@gmail.com>
2023-01-29 00:38:43 +0100

    Home page Set

--- src/Front-End/app.tsx
+++ src/Front-End/app.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { BiChevronRightCircle } from "react-icons/bi";
+import HomeImg from "../img/home.jpg"
 
 export default function App() {
   return (
-    <div>
-      Hello world
+    <>
+    <div className="nav">
+      <div className="nav1">
+        <h2>Marvy's Place</h2>
+        <ul>
+          <a href="" style={{ textDecoration: "none", color: "white" }}>Home</a>
+          <a href="" style={{ textDecoration: "none", color: "white" }}>Services</a>
+          <a href="" style={{ textDecoration: "none", color: "white" }}>Rooms</a>
+          <a href="" style={{ textDecoration: "none", color: "white"}}>About Us</a>
+        </ul>
+        <button>Join Member</button>
+      </div>
+      <div className="nav2">
+        <h1>
+          Giving the BEST <br /> Just for You
+        </h1>
+        <ul>
+          <a href="" style={{ textDecoration: "none", color: "white" }} className = "ullink">Book a Hotel Room <BiChevronRightCircle /></a>
+          <a href="" style={{ textDecoration: "none", color: "white" }}  className = "ullink">Call a Room Service <BiChevronRightCircle /></a>
+          <a href="" style={{ textDecoration: "none", color: "white" }} className = "ullink">View Hotel Services <BiChevronRightCircle /></a>
+        </ul>
+      </div>
     </div>
-  )
+      <img src={HomeImg} alt="Image"/>
+      </>
+  );
 }
--- src/Front-End/index.tsx
+++ src/Front-End/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {createRoot} from "react-dom/client"
 import App from "./app"
 import {BrowserRouter} from "react-router-dom"
+import "../index.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement)
