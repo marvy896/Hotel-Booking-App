1471177150005134a867608212ffdb4c56fbb9df
marvy896 <onyex896@gmail.com>
2023-02-10 09:51:03 +0100

    Currency Style updated

--- src/components/listRooms.tsx
+++ src/components/listRooms.tsx
@@ -12,13 +12,16 @@ export interface Roomss {
 
 export default function ListRooms({
     RoomId, NameOfRoom,Price, Image, Description}: Roomss) {
+      let newPrice = Price/100
+      let numberFormat = Intl.NumberFormat('en-ng', { style: 'currency', currency: 'NGN', currencyDisplay:"narrowSymbol" }).format(newPrice)
+      
   return (
     <div className="roomNums">
         <div>
          <div className="img" style={{backgroundImage: `url(${Image})`}}></div>
           <h4>{NameOfRoom}</h4>
           <p>{Description}</p>
-          <p>${Price}</p>
+          <p>{numberFormat}</p>
           <Link to="/booking?room=1" style={{ textDecoration: "none" }}>
             <div className="FirstDiv">
               Book{" "}
