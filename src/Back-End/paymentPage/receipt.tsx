b6cca34a8c6e37439d2e34576a19cb649af41152
marvy896 <onyex896@gmail.com>
2023-03-03 09:33:13 +0100

    Restrictions and Redirection

--- src/Back-End/paymentPage/receipt.tsx
+++ src/Back-End/paymentPage/receipt.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Payment from '../../Front-End/payment';
 
 export default function Receipt() {
   return (
     <div>
-      
+      <div>
+        <h1>Receipt of Payment</h1>
+        <h4>Date of Payment</h4>
+      </div>
     </div>
   )
 }
